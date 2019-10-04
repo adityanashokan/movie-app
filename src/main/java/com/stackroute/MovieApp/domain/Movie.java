3c6eb27c8242a0a549d7e137718f23d68fdc24af
adityan.a <adityan.a@cgi.com>
2019-10-04 09:37:34 +0530

    Used lombok in Movie class

--- src/main/java/com/stackroute/MovieApp/domain/Movie.java
+++ src/main/java/com/stackroute/MovieApp/domain/Movie.java
@@ -1,10 +1,19 @@
 package com.stackroute.MovieApp.domain;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Movie {
 
     @Id
@@ -15,66 +24,5 @@ public class Movie {
     private double vote_average;
 
 
-    public Movie() {
-    }
-
-    public Movie(int movieId, String movieName, String overview, String tagline, double vote_average) {
-        this.movieId = movieId;
-        this.movieName = movieName;
-        this.overview = overview;
-        this.tagline = tagline;
-        this.vote_average = vote_average;
-    }
-
-    @Override
-    public String toString() {
-        return "Movie{" +
-                "movieId=" + movieId +
-                ", movieName='" + movieName + '\'' +
-                ", overview='" + overview + '\'' +
-                ", tagline='" + tagline + '\'' +
-                ", vote_average=" + vote_average +
-                '}';
-    }
-
-    public int getMovieId() {
-        return movieId;
-    }
-
-    public void setMovieId(int movieId) {
-        this.movieId = movieId;
-    }
-
-    public String getMovieName() {
-        return movieName;
-    }
-
-    public void setMovieName(String movieName) {
-        this.movieName = movieName;
-    }
-
-    public String getOverview() {
-        return overview;
-    }
-
-    public void setOverview(String overview) {
-        this.overview = overview;
-    }
-
-    public String getTagline() {
-        return tagline;
-    }
-
-    public void setTagline(String tagline) {
-        this.tagline = tagline;
-    }
-
-    public double getVote_average() {
-        return vote_average;
-    }
-
-    public void setVote_average(double vote_average) {
-        this.vote_average = vote_average;
-    }
 
 }
